4649e7fa0d02af04dcc6decde37410121aaf8158
Bill <billcoblentz@gmail.com>
2021-12-18 17:03:20 -0700

    first commit

---
+++ Dojo_Pizzeria.js
@@ -0,0 +1,21 @@
+//Function to create an object for pizza making
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var pizza1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+console.log(pizza1);
+
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(pizza2);
+
+var pizza3 = pizzaOven("traditional", "ranch", "cheddar", "mushrooms");
+console.log(pizza3);
+
+var pizza4 = pizzaOven("deep dish", "ranch", "mozzarella", ["onions", "canadian beacon"]);
+console.log(pizza4);
